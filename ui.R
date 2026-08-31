0f742b38ad78b03acce1549d63ff11d2ae390004
Alexis Samp <alexis@totabc.com>
2026-08-31 13:37:31 -0700

    Updated UI

--- ui.R
+++ ui.R
@@ -1,46 +1,68 @@
 library(shiny)
-library(shinydashboard)
+library(bslib)
 
-# Define UI for application that draws a histogram
-ui <- dashboardPage(
-  dashboardHeader(title = "INSTO Dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      id = "tabs",
-      menuItem("Home", tabName = "home"),
-      menuItem("Water Management", tabName = "water"),
-      menuItem("Energy Management", tabName = "energy"),
-      menuItem("Solid Waste Management", tabName = "waste"),
-      menuItem("Climate Action", tabName = "climate")
-    )
+ui <- page_fluid(
+  
+  tags$head(
+    tags$style(HTML("
+    .top-content {
+      text-align: center;
+      margin-top: 30px;
+    }
+
+    .logo {
+      width: 600px;
+      max-width: 100%;
+      height: auto;
+    }
+
+      .big-card {
+        display: block;
+        max-width: 350px;
+        margin: 20px auto;
+        padding: 40px;
+        text-decoration: none;
+        color: inherit;
+        border: 1px solid #dee2e6;
+        border-radius: 12px;
+        text-align: center;
+        transition: 0.2s;
+      }
+
+      .big-card:hover {
+        text-decoration: none;
+        color: inherit;
+        transform: translateY(-3px);
+        box-shadow: 0 6px 18px rgba(0,0,0,0.15);
+      }
+    "))
   ),
   
-  dashboardBody(
-    tabItems(
-      tabItem(
-        tabName = "home"
-        
+  div(
+    class = "top-content",
+      tags$img(
+        src = "TOTA_logo.png",
+        class = "logo"
       ),
-      
-      tabItem(
-        tabName = "water",
-        p("Couple water indicators here (specifically useful for INSTO)"),
-        p("For more water info go here: ", tags$a(
-          href = "https://tota-water-dashboard.share.connect.posit.cloud/",
-          "TOTA WATER DASHBOARD"
-        )),
-        style = "font-size: 18px;"
+    
+    h1("INSTO HUB"),
 
-      ),
-      tabItem(
-        tabName = "energy"
-      ),
-      tabItem(
-        tabName = "waste"
-      ),
-      tabItem(
-        tabName = "climate"
-      )
+    tags$a(
+      href = "https://tota-water-dashboard.share.connect.posit.cloud/",
+      target = "_blank",
+      class = "big-card",
+      
+      h2("TOTA Water Dashboard"),
+      p("Click anywhere on this card to open the dashboard.")
+    ),
+    
+    tags$a(
+      href = "https://tota-water-dashboard.share.connect.posit.cloud/",
+      target = "_blank",
+      class = "big-card",
+      
+      h2("TOTA Solid Waste Dashboard"),
+      p("Click anywhere on this card to open the dashboard.")
     )
   )
 )
